668bbc0d7d7511870396f1ef5f92d45265acea7f
George Moses <67199528+Georgee0@users.noreply.github.com>
2021-12-27 11:25:45 +0100

    Create traficLight.py

---
+++ traficLight.py
@@ -0,0 +1,17 @@
+
+# Asseertion in trafic light stimulation
+
+market_2nd = {'ns':'green', 'ew':'red'}
+mission_16th = {'ns':'red', 'ew':'green'}
+
+def switchLights(stoplight):
+    for key in stoplight.keys():
+        if stoplight[key] == 'green':
+            stoplight[key] = 'yellow'
+        elif stoplight[key] == 'yellow':
+            stoplight[key] = 'red'
+        elif stoplight[key] == 'red':
+            stoplight[key] = 'green'     
+    assert 'red' in stoplight.values(), 'Neiher light is red' + str(stoplight)
+switchLights(market_2nd)            
+
